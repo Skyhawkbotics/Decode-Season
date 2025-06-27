52fc5012908c512b900ea860e1e162959995f506
Synaptic <devkrishnasahay@gmail.com>
2025-06-27 01:35:49 -0700

    Add paths to push more samples

--- TeamCode/src/main/java/Tests/netzoneauto.java
+++ TeamCode/src/main/java/Tests/netzoneauto.java
@@ -23,24 +23,41 @@ public class netzoneauto extends OpMode {
     private PathChain pushSample;
 
     private Pose startPose = new Pose(10, 110);
-    private Pose point1 = new Pose(63.2, 108);
-    private Pose netZone = new Pose(17,128);
-
+    private Pose sample1 = new Pose(63.2, 108);
+    private Pose netZone1 = new Pose(17,128);
+    private Pose sample2 = new Pose(70, 125);
+    private Pose netZone2 = new Pose(20,132);
+    private Pose sample3 = new Pose(64.5, 134.25);
+    private Pose netZone3 = new Pose(26.6,134.25);
     private Pose endZone = new Pose(17,17);
 
     private Path line1, line2;
 
     public void buildPaths() {
         pushSample = follower.pathBuilder()
-                .addPath(new BezierCurve(new Point(startPose), new Point(49, 111, Point.CARTESIAN), new Point(point1)))
-                    .setConstantHeadingInterpolation(0)
+                .addPath(new BezierCurve(new Point(startPose), new Point(49, 111, Point.CARTESIAN), new Point(sample1)))
+                    .setLinearHeadingInterpolation(0,Math.toRadians(-30))
 
-                .addPath(new BezierCurve(new Point(point1), new Point(32, 126, Point.CARTESIAN), new Point(netZone)))
+                .addPath(new BezierCurve(new Point(sample1), new Point(32, 126, Point.CARTESIAN), new Point(netZone1)))
                     .setTangentHeadingInterpolation()
                     .setReversed(true)
 
-                .addPath(new BezierCurve(new Point(netZone), new Point(30, 66, Point.CARTESIAN), new Point(endZone)))
-                    .setLinearHeadingInterpolation(Math.toRadians(-8), Math.toRadians(180))
+                .addPath(new BezierCurve(new Point(netZone1), new Point(63.5,105), new Point(sample2)))
+                    .setConstantHeadingInterpolation(Math.toRadians(30))
+
+                .addPath(new BezierCurve(new Point(sample2), new Point(45.5, 123.7, Point.CARTESIAN), new Point(netZone2)))
+                    .setTangentHeadingInterpolation()
+                    .setReversed(true)
+
+                .addPath(new BezierCurve(new Point(netZone2), new Point(44.4, 118.75), new Point(sample3)))
+                    .setLinearHeadingInterpolation(Math.toRadians(-18),0)
+
+                .addPath(new BezierLine(new Point(sample3), new Point(netZone3)))
+                    .setTangentHeadingInterpolation()
+                    .setReversed(true)
+
+                .addPath(new BezierLine(new Point(netZone3), new Point(endZone)))
+                    .setConstantHeadingInterpolation(0)
 
                 .build();
     }
